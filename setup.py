03581f7e39749c3aab724bbaa1609e65ec149c4c
Sebastien Perez <sebastien.perez@amadeus.com>
2015-11-02 18:25:52 +0100

    New release

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ requirements = [
 setup(
   name = 'travel-intelligence',
   packages = ['travel-intelligence'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   install_requires=requirements,
   description = 'A wrapper around the travel-intelligence Web Service API',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perez@amadeus.com',
   url = 'https://github.com/travel-intelligence/TIpy', # use the URL to the github repo
-  download_url = 'https://github.com/travel-intelligence/TIpy/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/travel-intelligence/TIpy/tarball/0.3', # I'll explain this in a second
   keywords = ['travel', 'intelligence', 'business', 'api', 'web services'], # arbitrary keywords
   classifiers = [],
 )
